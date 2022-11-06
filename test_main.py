bc749436ea9d0ccc827f2bbd42e446c6798d93f3
sritanaya <sritanaya33@gmail.com>
2022-11-07 04:40:10 +0530

    update2

--- test_main.py
+++ test_main.py
@@ -9,7 +9,7 @@ import main
 
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
+EMAIL = 'sritanaya33@gmail.com'
 PASSWORD = 'huff-puff'
 
 @pytest.fixture
